6112a0e0d458268fbb64a77880b0ca1e9e056391
bagci <bagciselami@hotmail.fr>
2020-02-04 02:51:38 +0100

    maj

--- config-server/src/main/java/com/p7/config/ConfigServerApplication.java
+++ config-server/src/main/java/com/p7/config/ConfigServerApplication.java
@@ -1,6 +1,7 @@
 package com.p7.config;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
