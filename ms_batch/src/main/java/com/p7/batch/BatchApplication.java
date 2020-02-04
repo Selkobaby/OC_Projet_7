dfd6732ba209357f538534d4edbf32e3bdfcd8f3
bagci <bagciselami@hotmail.fr>
2020-02-04 02:39:23 +0100

    ajout anotation configproperties

--- ms_batch/src/main/java/com/p7/batch/BatchApplication.java
+++ ms_batch/src/main/java/com/p7/batch/BatchApplication.java
@@ -2,8 +2,10 @@ package com.p7.batch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class BatchApplication {
 
 	public static void main(String[] args) {
