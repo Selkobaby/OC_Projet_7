8aec676382a9677d8065391899ec8475b1c931e5
bagci <bagciselami@hotmail.fr>
2020-02-04 02:50:53 +0100

    ajout anotation discovery client et config properties

--- ms_client/src/main/java/com/p7/client/ClientApplication.java
+++ ms_client/src/main/java/com/p7/client/ClientApplication.java
@@ -1,9 +1,15 @@
 package com.p7.client;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableConfigurationProperties
+@EnableAutoConfiguration
 public class ClientApplication {
 
 	public static void main(String[] args) {
