4a6c7654d1b1dbb76a861c6cbe11ed76519a531d
Selami <bagciselami@hotmail.fr>
2020-02-12 19:31:10 +0100

    ajout du service users

---
+++ ms_user/src/main/java/com/p7/msu/service/UsersService.java
@@ -0,0 +1,15 @@
+package com.p7.msu.service;
+
+import com.p7.msu.repository.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsersService {
+
+    @Autowired
+    private UsersRepository usersRepository;
+
+
+
+}
