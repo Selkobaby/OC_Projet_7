7120fd59e9cbe13e4c556512af03c8789c63a6e1
Selami <bagciselami@hotmail.fr>
2020-02-12 19:30:01 +0100

    ajout des class entité avec leurs attribut

---
+++ ms_client/src/main/java/com/p7/client/entity/Livres.java
@@ -0,0 +1,52 @@
+package com.p7.client.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "Livres")
+public class Livres {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Id")
+    private long id;
+
+    @Column(name = "Titre")
+    private String titre;
+
+    @Column(name = "Description")
+    private String description;
+
+    @Column(name = "Categorie")
+    @ManyToMany(fetch = FetchType.EAGER)
+    @OrderBy
+    @JoinTable(
+            joinColumns = @JoinColumn (name = "Livres_Id"),
+            inverseJoinColumns =  @JoinColumn (name = "Categorie_id")
+    )
+    @JsonIgnore
+    private Set <Categories> categorieLivre = new HashSet<Categories>();
+
+    @Column(name = "Auteur")
+    private String auteur;
+
+    @Column(name = "Image")
+    private String image;
+
+    @Column(name = "Disponible")
+    private boolean disponible;
+
+
+}
