e6336e45d50d976e3c4cb1a9179ffb1d995eb20a
bagci <bagciselami@hotmail.fr>
2020-02-14 01:51:05 +0100

    creation classe repository

--- ms_user/src/main/java/com/p7/msu/repository/UsersRepository.java
+++ ms_user/src/main/java/com/p7/msu/repository/UsersRepository.java
@@ -1,15 +1,13 @@
 package com.p7.msu.repository;
 
 import com.p7.msu.entity.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
-public interface UsersRepository extends CrudRepository<Users, Long> {
+public interface UsersRepository extends JpaRepository<Users, Long> {
 
-    @Query(value = "SELECT * FROM Users u WHERE u.email = ?", nativeQuery = true)
-    Optional<Users> authantificationUsers (String email);
+    @Query(value = "SELECT * FROM Users WHERE u.email = ?", nativeQuery = true)
+    public Optional<Users> findByEmail(String email);
 }
