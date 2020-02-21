de048b3de2ef54e4fce2c325670755366f366dae
bagci <bagciselami@hotmail.fr>
2020-02-21 01:52:00 +0100

    ajout controller pour un nouvelle utilisateur, un utilisateur qui s'enregistre et un utilisateur par rapport a son id

--- ms_user/src/main/java/com/p7/msu/controller/UsersController.java
+++ ms_user/src/main/java/com/p7/msu/controller/UsersController.java
@@ -16,40 +16,52 @@ public class UsersController {
     @Autowired
     private UsersService usersService;
 
-    @PostMapping("/users")
-    public Users save(@RequestBody Users users){
-        return usersService.addUsers();
-    }
-
     @GetMapping("/users/{id}")
     public Users getById(@PathVariable(value = "id") Long usersId){
         Users users = null;
         try{
-        users = usersService.getUsersById(usersId).orElseThrow( () ->
-                new UsersNotFoundException("Il n'y a pas d'utilsateur avec cet identifiant" +usersId));
-    } catch (Exception e){
-        System.out.println("Il n'y a pas d'utilisateur avec cet identifiant " + usersId + " " + e);
-    }
+            users = usersService.getUsersById(usersId).orElseThrow( () ->
+                    new UsersNotFoundException("Il n'y a pas d'utilsateur avec cet identifiant" +usersId));
+        } catch (Exception e){
+            System.out.println("Il n'y a pas d'utilisateur avec cet identifiant " + usersId + " " + e);
+        }
         return users;
     }
 
-    @GetMapping("/users")
-    public List<Users> getAll(){
-        return usersService.findAll();
+
+    @PostMapping("/sign in")
+    public Users signIn( @RequestParam("email") String email, @RequestParam("password") String password) {
+
+            Users users = null;
+            if(usersService.passwordOk(email, password)) {
+                try {
+                    users = usersService.usersByMail(email).orElseThrow(() ->
+                            new UsersNotFoundException("Ce mail n'esxite pas"+email)
+                    );
+                } catch(Exception e) {
+                   System.out.println("Ce mail n'esxite pas "+email+" "+e);
+
+                }
+            } else {
+                System.out.println("L'utilisateur ou le mot de passe est mauvais");
+            }
+            return users;
+        }
+
+    @PostMapping(value = {"/newUsers"})
+    public void setPerson(
+            @RequestParam("prenom") String nom,
+            @RequestParam("nom") String prenom,
+            @RequestParam("email") String email,
+            @RequestParam("password") String password
+    ) {
+        Users users= new Users();
+        users.setNom(nom);
+        users.setPrenom(prenom);
+        users.setEmail(email);
+        users.setPassword(password);
+        users.setAdmin(false);
+        usersService.addUsers(users);
     }
 
-    @DeleteMapping("/users/{id}")
-    public void deleteById(@PathVariable(value = "id") Long id){
-        usersService.deleteUsers();
-    }
-
-    @DeleteMapping("/users")
-    public void deleteAll(){
-        usersService.deleteAll();
-    }
-
-    @GetMapping("/users/count")
-    public long count(){
-        return usersService.count();
-    }
 }
